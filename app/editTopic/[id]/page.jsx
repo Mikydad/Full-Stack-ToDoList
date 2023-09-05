3634a0f1c8467c992166bf48756f6fcc6f8d4158
Milkesa Dereje <miki@pop-os.localdomain>
2023-09-05 06:46:18 +0300

    description error solved

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,8 +1,27 @@
 import EditTopicForm from '@/components/EditTopicForm'
 import React from 'react'
 
-const page = () => {
-  return <EditTopicForm />
+const getTopicById = async (id) => {
+  try{
+  const res = await fetch(`http://localhost:/3000/api/topics/${id}`, {
+    cache: "no-store"
+  })
+
+  if(!res.ok) {
+    throw new Error("Faild to fetch topic");
+  }
+
+  return res.json();
+}catch(error){
+  console.log(error)
+}
+}
+const EditTopic = async ({ params }) => {
+  const { id } = params;
+  const {topic} = await getTopicById(id)
+  const { title, description } = topic;
+
+  return <EditTopicForm id={id} title={title} description={description}/>
 }
 
-export default page
+export default EditTopic
